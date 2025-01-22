3bc3a1e0a7f3c560d0897eedd0bbd2e74cb33229
AiswaryaAjith <ajithashwarya@gmail.com>
2025-01-22 10:03:37 +0530

    Add files via upload

---
+++ FibonacciGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciGeneratorApp.Model
+{
+    public class FibonacciGenerator //class namespace
+    {
+        // Default constructor
+        public FibonacciGenerator() { }
+
+        // Method to generate the Fibonacci sequence
+        public int[] GenerateFibonacci(int n)
+        {
+            int[] fibonacciSequence = new int[n];
+
+            if (n >= 1) fibonacciSequence[0] = 0; // First Fibonacci number
+            if (n >= 2) fibonacciSequence[1] = 1; // Second Fibonacci number
+
+            for (int i = 2; i < n; i++)
+            {
+                fibonacciSequence[i] = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
+            }
+
+            return fibonacciSequence;
+        }
+
+        // Method to display the Fibonacci sequence
+        public void DisplayFibonacci(int n)
+        {
+            int[] sequence = GenerateFibonacci(n);
+
+            Console.WriteLine($"The first {n} numbers in the Fibonacci sequence are:");
+            foreach (int num in sequence)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine(); // For a new line after the sequence
+        }
+    }
+}
+
+
---
+++ FibonacciProgram.cs
@@ -0,0 +1,31 @@
+﻿using FibonacciGeneratorApp.Model;
+
+namespace ConsoleAppFibonacci
+{
+    internal class FibonacciProgram
+    {
+        static void Main(string[] args)
+        {
+            // Instance of the FibonacciGenerator class
+            FibonacciGenerator fibonacciGenerator = new FibonacciGenerator();
+
+            // Prompt the user for the number of Fibonacci numbers to display
+            Console.WriteLine("Enter the number of Fibonacci numbers to display:");
+            int n = int.Parse(Console.ReadLine());
+
+            // Validate input
+            if (n <= 0)
+            {
+                Console.WriteLine("Please enter a positive integer greater than 0.");
+            }
+            else
+            {
+                // Display the Fibonacci sequence using the DisplayFibonacci method
+                fibonacciGenerator.DisplayFibonacci(n);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
