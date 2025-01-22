aeb10e1abb0bc97030ae6394f8274d54d6ee14c0
AiswaryaAjith <ajithashwarya@gmail.com>
2025-01-22 10:02:47 +0530

    Add files via upload

---
+++ DigitSumProgram.cs
@@ -0,0 +1,23 @@
+﻿using DigitSumApp.Model;
+
+namespace ConsoleAppDigitSum
+{
+    internal class DigitSumProgram
+    {
+        static void Main(string[] args)
+        {
+            // Instance of the DigitSumCalculator class
+            DigitSumCalculator digitSumCalculator = new DigitSumCalculator();
+
+            // Prompt the user for a number
+            Console.WriteLine("Enter a number to calculate the sum of its digits:");
+            int number = int.Parse(Console.ReadLine());
+
+            // Display the digit sum using the DisplayDigitSum method
+            digitSumCalculator.DisplayDigitSum(number);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
