fd401241905d215efd77fcabddbb30bfc7b7b83a
AiswaryaAjith <ajithashwarya@gmail.com>
2025-01-22 10:04:20 +0530

    Add files via upload

---
+++ SpaceProgram.cs
@@ -0,0 +1,24 @@
+﻿using StringSpaceCounterApp.Model;
+
+namespace StringSpaceCounterApp
+{
+    internal class SpaceProgram
+    {
+        static void Main(string[] args)
+        {
+            // Instance of the StringSpaceCounter class
+            StringSpaceCounter spaceCounter = new StringSpaceCounter();
+
+            // Prompt the user for a string
+            Console.WriteLine("Enter a string:");
+            string userInput = Console.ReadLine();
+
+            // Display the result using the DisplaySpaceCount method
+            spaceCounter.DisplaySpaceCount(userInput);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
+
